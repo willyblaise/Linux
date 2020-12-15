d51a63c4a261e719ae219002747f825c2e1d1c31
William <kravendahunter@hotmail.com>
2020-12-14 23:14:45 -0600

    improved my custom script for doing things fast

---
+++ scripts/custom.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+se(){
+	du -a ~/.scripts/* ~/.config/* | awk '{ print $2 }' | fzf | xargs -r $EDITOR;
+	#du -a ~/.scripts/* ~/.config/* | awk '{ print $2 }' | fzf | xargs -r nvim;
+}
+
+up(){
+	city=$1
+
+	if [ -z $city ];then
+		unset city
+	fi
+
+	curl wttr.in/$city
+}
+
+rate(){
+	coin=$1
+
+	if [ -z $coin ];then
+		unset coin
+	fi
+
+	curl rate.sx/$coin
+}
+
+sp(){
+	speedtest-cli
+}
+
+wpa(){
+	sudo wpa_supplicant -B -i wlp3s0 -c /etc/wpa_supplicant/wpa_supplicant-wlp3s0.conf
+}
+
+swap(){
+	sudo swapon /swapfile
+}
